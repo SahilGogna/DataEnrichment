1518d2b9299b919e568c7ffc368cfedc022bbfca
Sahil Gogna <gogna.sahil95@gmail.com>
2020-03-10 21:25:05 -0400

    filter added for monday metro trips

--- src/com/mcit/scala/AppStarter/EntryPoint.scala
+++ src/com/mcit/scala/AppStarter/EntryPoint.scala
@@ -24,7 +24,7 @@ object EntryPoint extends App {
   val enrichedTrip: List[EnrichedTrip] = enrichedTripRoute.map(tripRoute => EnrichedTrip(tripRoute,
                                                     calenderLookUp.lookup(tripRoute.trips.service_id)))
 
-  val finalListOfTrips: List[EnrichedTrip] = enrichedTrip.filter(a => a.calender.monday.equals("1"))
+  val finalListOfTrips: List[EnrichedTrip] = enrichedTrip.filter(a => a.calender.monday.equals("1") && a.tripRoute.routes.route_type.equals("1"))
   val writer = new DataWriter(finalListOfTrips)
   writer.writeData()
 }
